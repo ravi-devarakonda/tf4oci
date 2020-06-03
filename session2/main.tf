188d5256218fc7398245afaa6fc2a1fcee27c5bb
rsdevara <rsdevara@RSDEVARA-IN.in.oracle.com>
2020-06-03 18:34:18 +0530

    formatted to indent

--- session2/main.tf
+++ session2/main.tf
@@ -1,28 +1,32 @@
 #instance provision
 resource "oci_core_instance" "test_instance" {
-    count = var.ins_count
-    #Required
-    availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-    compartment_id = var.compartment
-    shape = var.shape
-    display_name = "${var.displayname}-${count.index+1}"
+  count = var.ins_count
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  compartment_id      = var.compartment
+  shape               = var.shape
+  display_name        = "${var.displayname}-${count.index + 1}"
 
-    create_vnic_details {
-        #Required
-        subnet_id = var.subnet_id
-        hostname_label = "${var.displayname}-${count.index+1}"
-    }
-    metadata = {
-        ssh_authorized_keys = var.ssh_public_key
-    }
-    source_details {
-        #Required
-        source_id = var.image_id
-        source_type = "image"
-    }
-    preserve_boot_volume = false
+  create_vnic_details {
+    #Required
+    subnet_id      = var.subnet_id
+    hostname_label = "${var.displayname}-${count.index + 1}"
+  }
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
+  source_details {
+    #Required
+    source_id   = var.image_id
+    source_type = "image"
+  }
+  preserve_boot_volume = false
 }
 
-output "instance_details" {
-    value = oci_core_instance.test_instance.*
+output "private_ips" {
+  value = oci_core_instance.test_instance.*.private_ip
+}
+
+output "public_ips" {
+  value = oci_core_instance.test_instance.*.public_ip
 }
--- session2/variables.tf
+++ session2/variables.tf
@@ -5,38 +5,38 @@ variable fingerprint {}
 variable private_key_path {}
 variable ssh_public_key {}
 variable region {
-    type = string
-    default = "us-ashburn-1"
+  type    = string
+  default = "us-ashburn-1"
 }
 
 #variables for instance provision
 
 variable compartment {
-    type = string
-    default = "ocid1.compartment.oc1..aaaaaaaadk2lcuzqspj4jj6wts43mjimym5qrhqqt6nzcz6hqrehjjzungxq"
+  type    = string
+  default = "ocid1.compartment.oc1..aaaaaaaadk2lcuzqspj4jj6wts43mjimym5qrhqqt6nzcz6hqrehjjzungxq"
 }
 
 variable subnet_id {
-    type = string
-    default = "ocid1.subnet.oc1.iad.aaaaaaaaw737zp2ily2vh2zngv4zwfaej64j337xvzwdj2hnc7wd2dku52sq"
+  type    = string
+  default = "ocid1.subnet.oc1.iad.aaaaaaaaw737zp2ily2vh2zngv4zwfaej64j337xvzwdj2hnc7wd2dku52sq"
 }
 
 variable displayname {
-    type = string
-    default = "RD-Default-VM"
+  type    = string
+  default = "RD-Default-VM"
 }
 
 variable ins_count {
-    type = string
-    default = "1"
+  type    = string
+  default = "1"
 }
 
 variable shape {
-    type = string
-    default = "VM.Standard2.2"
+  type    = string
+  default = "VM.Standard2.2"
 }
 
 variable image_id {
-    type = string
-    default = "ocid1.image.oc1.iad.aaaaaaaahjkmmew2pjrcpylaf6zdddtom6xjnazwptervti35keqd4fdylca"
+  type    = string
+  default = "ocid1.image.oc1.iad.aaaaaaaahjkmmew2pjrcpylaf6zdddtom6xjnazwptervti35keqd4fdylca"
 }
